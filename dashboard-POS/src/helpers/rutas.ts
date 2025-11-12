d4ab5546171e14d9f77805c5c2463ba836fe47d1
CamiloC7 <j.camilocorredor@gmail.com>
2025-11-11 23:15:16 -0500

    cambios en dashboard-pos

--- dashboard-POS/src/helpers/rutas.ts
+++ dashboard-POS/src/helpers/rutas.ts
@@ -1,4 +1,4 @@
- //export const RUTA = "http://localhost:3002";
-export const RUTA = "https://gastro-pos.qualitysoftservices.com";
+ export const RUTA = "http://localhost:3002";
+// export const RUTA = "https://gastro-pos.qualitysoftservices.com";
 
 export const BASE_URL = "https://facturador.qualitysoftservices.com/";
